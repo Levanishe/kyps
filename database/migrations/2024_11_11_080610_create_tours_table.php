70fa6c0352b617f5ba5146736c46350240542117
Levan <levanmamasahlisi@gmail.com>
2024-11-11 13:43:12 +0500

    no first commit

--- database/migrations/2024_11_11_080610_create_tours_table.php
+++ database/migrations/2024_11_11_080610_create_tours_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->text('description');
             $table->decimal('price', 8, 2);
             $table->decimal('left');
-            //dsd
             $table->timestamps();
         });
     }
